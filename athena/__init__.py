42e49559736c5bc3777fe507898343087897fb8a
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-06-11 00:27:43 +0300

    Update __init__.py

--- athena/__init__.py
+++ athena/__init__.py
@@ -1,1 +1,12 @@
+"""
+ATHENA Scalar-Disformal Vacuum Field Theory Package
+Versiyon: 25.13
+"""
 
+from .field_solver import solve_phi
+# from .rotation_curves import rotation_curve  # varsa
+
+__version__ = "25.13"
+__author__ = "Mustafa Gökhan Yılmaz & Team"
+
+print(f"ATHENA v{__version__} yüklendi.")
